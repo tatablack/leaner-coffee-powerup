169379d6e730d3e74fe3adfe053ef81660a56fb8
Angelo Tata <angelo.tata@gmail.com>
2018-06-22 12:08:44 +0000

    Extend WebPack configuration to support filename hashes and env config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import LeanCoffeePowerUp from './LeanCoffeePowerUp';
 
-const HOSTNAME = '';
+const HOSTNAME = process.env.CONFIG[process.env.NODE_ENV].hostname;
 const DEFAULT_DISCUSSION_DURATION = 5 * 60 * 1000;
 
 /* global TrelloPowerUp */
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,31 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const Config = require("./config.js");
+
+const isProduction = process.env.NODE_ENV === 'production';
+const modulesPlugin = (isProduction ?
+  new webpack.HashedModuleIdsPlugin({
+    hashFunction: 'sha256',
+    hashDigest: 'hex',
+    hashDigestLength: 20
+  }) :
+  new webpack.NamedModulesPlugin())
 
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: isProduction ? 'source-map' : 'source-map',
+  
+  entry: './src/index.js',
+
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
 
   module: {
     rules: [{
@@ -15,6 +38,16 @@ module.exports = {
   },
 
   plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+      CONFIG: Config
+    }),
+    modulesPlugin,
+    new HtmlWebpackPlugin({
+      title: 'Lean Coffee Trello Power-up',
+      template: 'index.html'
+    }),
     new UglifyJsPlugin({ sourceMap: true })
   ]
 };
