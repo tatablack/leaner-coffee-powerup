ce349eb8e7902983f8d0ae776cf17cb82359d0dc
Angelo Tata <angelo.tata@skyscanner.net>
2018-06-26 21:23:30 +0100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import LeanCoffeePowerUp from './LeanCoffeePowerUp';
 const { hostname, port, prefix } = process.env.CONFIG[process.env.NODE_ENV];
 const DEFAULT_DISCUSSION_DURATION = 5 * 60 * 1000;
 
-/* global TrelloPowerUp */
 /* eslint-disable prefer-template */
 const instance = new LeanCoffeePowerUp({
-  TrelloPowerUp,
+  TrelloPowerUp: window.TrelloPowerUp,
   baseUrl: `${hostname}${port ? ':' + port : ''}${prefix}`,
   maxDiscussionDuration: DEFAULT_DISCUSSION_DURATION
 });
