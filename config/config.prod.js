75f5c9dd7dd9f0353dd88b307fca4edcbd42a8f1
Angelo Tata <angelo.tata@skyscanner.net>
2019-12-22 23:25:39 +0100

    Add Italian listing text

--- config/config.prod.js
+++ config/config.prod.js
@@ -2,6 +2,6 @@ module.exports = {
   production: {
     hostname: 'https://leaner-coffee.tatablack.net',
     defaultDuration: 5 * 60 * 1000,
-    supportedLocales: ['en']
+    supportedLocales: ['en', 'it']
   }
 };
