0e3551a807d796bfe8311077635b67157d3284c1
Angelo Tata <angelo.tata@gmail.com>
2025-05-01 14:19:04 +0100

    Add support for newer ESLint and prettier. Also switch to pnpm.

---
+++ eslint.config.mjs
@@ -0,0 +1,131 @@
+import js from "@eslint/js";
+import markdown from "@eslint/markdown";
+import json from "@eslint/json";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginImportX from "eslint-plugin-import-x";
+import * as tsParser from "@typescript-eslint/parser";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+
+export default tseslint.config(
+  { ignores: ["assets", "docs"] },
+  {
+    files: ["**/*.md"],
+    plugins: {
+      markdown,
+    },
+    extends: [...markdown.configs.recommended, eslintConfigPrettier],
+  },
+  {
+    files: ["**/*.json"],
+    language: "json/json",
+    plugins: {
+      json,
+    },
+    extends: [json.configs.recommended, eslintConfigPrettier],
+  },
+  {
+    files: ["src/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      eslintPluginImportX.flatConfigs.recommended,
+      eslintPluginImportX.flatConfigs.typescript,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+      parser: tsParser,
+      sourceType: "module",
+    },
+    settings: {
+      "import-x/resolver-next": [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+          project: "tsconfig.json",
+        }),
+      ],
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "import-x/no-dynamic-require": "warn",
+      "import-x/no-nodejs-modules": "warn",
+      "import-x/newline-after-import": ["error", { count: 1 }],
+      "import-x/order": [
+        "error",
+        {
+          groups: [
+            // Imports of builtins are first
+            "builtin",
+            "external",
+
+            // Then sibling and parent imports. They can be mingled together
+            ["sibling", "parent"],
+            // Then index file imports
+            "index",
+            // Then any arcane TypeScript imports
+            "object",
+            // Then the omitted imports: internal, type, unknown
+            "type",
+          ],
+          alphabetize: { order: "asc" },
+          sortTypesGroup: true,
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+  {
+    files: ["tools/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      eslintPluginImportX.flatConfigs.recommended,
+      eslintPluginImportX.flatConfigs.typescript,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.node,
+      parser: tsParser,
+      sourceType: "module",
+    },
+    settings: {
+      "import-x/resolver-next": [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+          project: "tsconfig.json",
+        }),
+      ],
+    },
+    rules: {
+      "import-x/no-dynamic-require": "warn",
+      "import-x/no-nodejs-modules": "off",
+      "import-x/newline-after-import": ["error", { count: 1 }],
+      "import-x/order": [
+        "error",
+        {
+          groups: [
+            // Imports of builtins are first
+            "builtin",
+            "external",
+
+            // Then sibling and parent imports. They can be mingled together
+            ["sibling", "parent"],
+            // Then index file imports
+            "index",
+            // Then any arcane TypeScript imports
+            "object",
+            // Then the omitted imports: internal, type, unknown
+            "type",
+          ],
+          alphabetize: { order: "asc" },
+          sortTypesGroup: true,
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+);
