f219018a2483f0f51b50c12403ccff36a9d09cc8
Angelo Tata <angelo.tata@gmail.com>
2025-05-03 22:10:23 +0100

    Switch from bundling Sentry to lazy loading it everywhere

--- powerup/src/discussion-ui.ts
+++ powerup/src/discussion-ui.ts
@@ -1,10 +1,6 @@
-import * as Sentry from "@sentry/browser";
-
 import LeanCoffeeDiscussionUI from "./LeanCoffeeDiscussionUI";
-import { SentryDefaultOptions } from "./utils/Sentry";
 
 const config: Config = process.env.CONFIG as unknown as Config;
-Sentry.init(SentryDefaultOptions);
 
 const discussionUI = new LeanCoffeeDiscussionUI({ w: window, config });
 discussionUI.init();
