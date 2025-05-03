8969ceb709e2453fb2dc910b77e52a1bc5cbbc8b
Angelo Tata <angelo.tata@gmail.com>
2025-05-03 12:44:08 +0100

    Use .env files for configuration

--- powerup/config/config.dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  development: {
-    hostname: "https://localhost",
-    port: 8080,
-    defaultDuration: 5 * 60 * 1000,
-    supportedLocales: ["en", "it"],
-  },
-};
--- powerup/webpack.prod.js
+++ powerup/webpack.prod.js
@@ -1,26 +1,15 @@
-const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { merge } = require("webpack-merge");
 
-const PACKAGE_JSON = require("./package.json");
 const common = require("./webpack.common.js");
 
-const Config = require("./config/config.prod.js");
-
 module.exports = merge(common, {
-  mode: "production",
   devtool: "source-map",
 
   plugins: [
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: ["**/*", "!CNAME"],
-    }),
-
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: "production",
-      CONFIG: Config,
-      VERSION: PACKAGE_JSON.version,
     }),
   ],
 
